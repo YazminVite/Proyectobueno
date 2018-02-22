068c3637c4308577babe759508adc09806db333e
YazminVite <00259491@red.unid.mx>
2018-02-22 14:04:57 -0600

    elbueno

---
+++ config.py
@@ -0,0 +1,14 @@
+import web
+
+db_host = 'l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'final'
+db_user = 'u5deyv5rb075qenk'
+db_pw = 'ub7nhidylwbedo7q'
+
+db = web.database(
+    dbn='mysql',
+    host=db_host,
+    db=db_name,
+    user=db_user,
+    pw=db_pw
+    )
